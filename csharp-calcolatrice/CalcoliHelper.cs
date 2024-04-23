13f966cd5699b626ec3ae57954721d2dfa83cb7a
IIODERII <oderrisi@gmail.com>
2024-04-23 15:32:27 +0200

    exercise completed

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    public static class CalcoliHelper
+    {
+        public static int Somma (int n1,  int n2) => n1+n2;
+        public static double Somma (double n1, double n2) => n1+n2;
+
+        public static int Differenza (int n1, int n2) => n1-n2;
+        public static double Differenza(double n1, double n2) => n1 - n2;
+
+        public static int Moltiplicazione (int n1, int n2) => n1*n2;
+        public static double Moltiplicazione(double n1, double n2) => n1 * n2;
+
+        public static int Assoluto(int n)
+        {
+            if(n < 0)
+            {
+                return n - (2 * n); 
+            }
+            return n;
+        }
+        public static double Assoluto(double n)
+        {
+            if (n < 0)
+            {
+                return n - (2 * n);
+            }
+            return n;
+        }
+
+        public static int Minore (int n1, int n2) => (n1 < n2) ? n1 : n2;
+        public static double Minore(double n1, double n2) => (n1 < n2) ? n1 : n2;
+
+        public static int Maggiore(int n1, int n2) => (n1 > n2) ? n1 : n2;
+        public static double Maggiore(double n1, double n2) => (n1 > n2) ? n1 : n2;
+
+        //Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi si chiama POLIMORFISMO
+
+        //BONUS
+
+        public static double Potenza(int baseN, int esponente)
+        {
+            if (esponente == 0)
+                return 1;
+
+            
+            int tot = 1;
+            for (int i = 0; i < CalcoliHelper.Assoluto(esponente); i++)
+            {
+                tot*= baseN;
+            }
+            if (esponente < 0)
+                return (double)1/tot;
+
+            return tot;
+
+        }
+    }
+}
--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -4,7 +4,131 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Benvenuto nella Calcolatrice!");
+
+            string input = "";
+            while (input != "end")
+            {
+                Console.Write("Scrivere:\n- N+N per l'Addizione;\n- NmenoN per la Sottrazione;\n- N*N per la Moltipicazione;\n- absN per il Valore Assoluto;\n- maxN,N per il Valore Maggiore tra due numeri;\n- minN,N per il Valore Minore tra due numeri;\n- B^E per la Potenza;\n- end per terminare il programma. \n\nInserisci la tua operazione: ");
+                input = Console.ReadLine();
+
+                if (input.Contains('+'))
+                {
+                    int index = input.IndexOf('+');
+
+                    string sub1 = input.Substring(0, index);
+                    string sub2 = input.Substring(index + 1);
+
+                    if (input.Contains('.') || input.Contains(','))
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Somma(double.Parse(sub1, System.Globalization.CultureInfo.InvariantCulture), double.Parse(sub2, System.Globalization.CultureInfo.InvariantCulture))}\n");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Somma(int.Parse(sub1), int.Parse(sub2))}\n");
+                    }
+                }
+                else if (input.Contains("meno"))
+                {
+                    int index = input.IndexOf('m');
+                    int indexLast = input.IndexOf('o');
+
+                    string sub1 = input.Substring(0, index);
+                    string sub2 = input.Substring(indexLast + 1);
+
+                    if (input.Contains('.') || input.Contains(','))
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Differenza(double.Parse(sub1, System.Globalization.CultureInfo.InvariantCulture), double.Parse(sub2, System.Globalization.CultureInfo.InvariantCulture))}\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Differenza(int.Parse(sub1), int.Parse(sub2))}\n");
+                    }
+                }
+                else if (input.Contains('*'))
+                {
+                    int index = input.IndexOf('*');
+
+                    string sub1 = input.Substring(0, index);
+                    string sub2 = input.Substring(index + 1);
+
+                    if (input.Contains('.') || input.Contains(','))
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Moltiplicazione(double.Parse(sub1, System.Globalization.CultureInfo.InvariantCulture), double.Parse(sub2, System.Globalization.CultureInfo.InvariantCulture))}\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Moltiplicazione(int.Parse(sub1), int.Parse(sub2))}\n");
+                    }
+                }
+                else if (input.Contains('^'))
+                {
+                    int index = input.IndexOf('^');
+
+                    string sub1 = input.Substring(0, index);
+                    string sub2 = input.Substring(index + 1);
+
+                    Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Potenza(int.Parse(sub1), int.Parse(sub2))}\n");
+                }
+                else if (input.Contains("abs"))
+                {
+                    int index = input.IndexOf('s');
+
+                    string sub = input.Substring(index + 1);
+
+                    if (input.Contains('.') || input.Contains(','))
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Assoluto(double.Parse(sub, System.Globalization.CultureInfo.InvariantCulture))}\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Assoluto(int.Parse(sub))}\n");
+                    }
+                }
+                else if (input.Contains("max"))
+                {
+                    int index = input.IndexOf(',');
+                    int indexX = input.IndexOf("x");
+
+                    string sub1 = input.Substring(indexX + 1, 1);
+                    string sub2 = input.Substring(index + 1);
+                    if (sub1 == "-")
+                        sub1 = input.Substring(indexX + 2, 1);
+                    if (input.Contains('.'))
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Maggiore(double.Parse(sub1, System.Globalization.CultureInfo.InvariantCulture), double.Parse(sub2, System.Globalization.CultureInfo.InvariantCulture))}\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Maggiore(int.Parse(sub1), int.Parse(sub2))}\n");
+                    }
+                }
+                else if (input.Contains("min"))
+                {
+                    int index = input.IndexOf(',');
+                    int indexX = input.IndexOf("n");
+
+                    string sub1 = input.Substring(indexX+1, 1);
+                    string sub2 = input.Substring(index + 1);
+                    if(sub1 == "-")
+                         sub1 = input.Substring(indexX + 2, 1);
+                    if (input.Contains('.'))
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Minore(double.Parse(sub1, System.Globalization.CultureInfo.InvariantCulture), double.Parse(sub2, System.Globalization.CultureInfo.InvariantCulture))}\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nIl risultato di {input} è {CalcoliHelper.Minore(int.Parse(sub1), int.Parse(sub2))}\n");
+                    }
+                }
+                else if (input == "end")
+                    break;
+                else
+                {
+                    Console.WriteLine("\nValore inserito non valido, riprovare!\n");
+                }
+            }
         }
     }
 }
